ba9fd8593cf320acbd0e43eb476e1b4a55cbdf62
jgaston93 <jgaston93@gmail.com>
2024-12-24 02:15:04 +0000

    adding input parsing

--- src/2024/Day16Solver.cpp
+++ src/2024/Day16Solver.cpp
@@ -1,6 +1,8 @@
 #include "Day16Solver.hpp"
 
-#include "AoCSolverCommon.hpp"
+#include <cstring>
+
+using namespace AOC_SOLVER_COMMON;
 
 namespace YEAR_2024::DAY_16
 {
@@ -12,16 +14,32 @@ namespace YEAR_2024::DAY_16
     if (fp != nullptr)
     {
       char line[AOC_SOLVER_COMMON::MAX_LINE_LENGTH] = {0};
+      int row = 0;
       while (!feof(fp))
       {
         fgets(line, sizeof(line), fp);
+        StripNewlines(line);
+        if (m_map_size == 0)
+        {
+          m_map_size = strlen(line);
+        }
+        strcpy(m_map[row++], line);
       }
       fclose(fp);
+
+      for (int i = 0; i < m_map_size; i++)
+      {
+        printf("%s\n", m_map[i]);
+      }
     }
   }
 
   void Solver::Run()
   {
   }
+
+  void Move(int x, int y, Direction dir, int score)
+  {
+    }
 
 } // namespace YEAR_2024::DAY_16
--- src/2024/Day16Solver.hpp
+++ src/2024/Day16Solver.hpp
@@ -3,8 +3,16 @@
 
 #include "ISolver.hpp"
 
+#include "AoCSolverCommon.hpp"
+
+#include <limits>
+
+using namespace AOC_SOLVER_COMMON;
+
 namespace YEAR_2024::DAY_16
 {
+
+  const int MAX_MAP_SIZE = 141;
   class Solver : public ISolver
   {
   public:
@@ -12,6 +20,10 @@ namespace YEAR_2024::DAY_16
     void Run() final;
 
   private:
+    void Move(int x, int y, Direction dir, int score);
+    int m_map_size = 0;
+    char m_map[MAX_MAP_SIZE][MAX_MAP_SIZE] = {0};
+    int m_score_map[MAX_MAP_SIZE][MAX_MAP_SIZE] = {std::numeric_limits<int>::max()};
   };
 } // namespace YEAR_2024::DAY_16
 
